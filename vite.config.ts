57e39bee2899e821f440bcf1882c2b85685b9cd6
ChrisLoucaides <christosloucaides@gmail.com>
2025-08-17 14:27:58 +0100

    Create pinia store

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/smash-eu-stage-striker/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'],
+        },
+      },
+    },
+  },
 })
